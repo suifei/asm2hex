e3d593675f64eec08d78a6cfa046af0b7e6bc07d
suifei <suifei@me.com>
2024-05-22 02:40:39 +0800

    Update

--- win_build.bat
+++ win_build.bat
@@ -1,5 +1,8 @@
 @echo off
+set PATH=%PATH%;D:\msys64\mingw64\bin
 set CGO_CFLAGS="-ID:/works/asm2hex/link/win64/include"
 set CGO_LDFLAGS="-LD:/works/asm2hex/link/win64/lib -lcapstone -lkeystone"
 fyne package
+copy /Y link\win64\bin\capstone.dll .
+copy /Y link\win64\bin\keystone.dll .
 echo "Build complete."
